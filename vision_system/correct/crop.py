689f5e433e898be715556ca3c16c0b425953c51d
wasilewskiJ <wasilewski.jakub@outlook.com>
2024-03-22 23:09:02 +0100

    Update crop.py - added function description

--- vision_system/correct/crop.py
+++ vision_system/correct/crop.py
@@ -2,7 +2,21 @@ import cv2
 import os
 
 def crop_photo(img, DIR_PATH='./vision_system/correct', OUTPUT=False, OUTPUT_NAME='ready_img.png'):
+    """
+    Crop the input image based on predefined pixel values and optionally save the cropped image.
+
+    Args:
+        img (numpy.ndarray): Image to be cropped.
+        DIR_PATH (str): Directory to save the cropped image. Defaults to './vision_system/correct'.
+        OUTPUT (bool): Flag to save the cropped image. Defaults to False.
+        OUTPUT_NAME (str): Filename for the cropped image. Defaults to 'ready_img.png'.
+
+    Returns:
+        numpy.ndarray: The cropped image.
     
+    The function crops the image using a hardcoded pixel array specifying the crop margins and
+    saves the cropped image if OUTPUT is True. It prints a message when the image is successfully cropped.
+    """
     OUTPUT_PATH = os.path.join(DIR_PATH, OUTPUT_NAME) 
     
     # Amount of pixels to crop: [top, bottom, left, right]
@@ -20,7 +34,7 @@ def crop_photo(img, DIR_PATH='./vision_system/correct', OUTPUT=False, OUTPUT_NAM
 
     # Save the cropped image if asked
     if OUTPUT:
-    	cv2.imwrite(output_path, cropped_img)
+        cv2.imwrite(OUTPUT_PATH, cropped_img)
     
     print(f"Cropped image correctly")
 
