00775fa4cac2d75c7e8d5561489ae1b62c593007
wasilewskiJ <wasilewski.jakub@outlook.com>
2024-03-22 23:45:19 +0100

    added functions descriptions

--- move_pour.py
+++ move_pour.py
@@ -4,10 +4,12 @@ import subprocess
 from math import dist
 import numpy as np
 
+#Initial configuration
 HOST = "localhost"
 PORT = 7125
 API_KEY = "56831fa1de8f4676a6be1b703c1bdf94"
 
+#Robot configuration
 max_x = 579
 max_y = 544
 F = 13000
@@ -17,6 +19,16 @@ POUR_TIME = 5.5 #seconds
 emergency_stop = False
 
 def run_gcode(gcode):
+    """
+    Executes a G-code command on the robot via the API.
+
+    Args:
+        gcode (str): The G-code command to execute.
+
+    Returns:
+        dict: The response from the server in JSON format.
+    """
+
     try:
         r = requests.post(f"http://{HOST}:{PORT}/printer/gcode/script?script={gcode}", headers={"X-Api-Key": API_KEY}, timeout=20)
     except requests.exceptions.ConnectionError:
@@ -40,6 +52,13 @@ def run_gcode(gcode):
     return r
 
 def get_status():
+    """
+    Retrieves the current status of the robot.
+
+    Returns:
+        dict: Information about the printer's status in JSON format.
+    """
+
     r = requests.get(f"http://{HOST}:{PORT}/server/info", headers={"X-Api-Key": API_KEY})
     return r.json()
 
@@ -49,6 +68,13 @@ def stop():
 
 #wait until robot is accesible
 def wait():
+    """
+    Waits for the printer to complete the current task.
+
+    Returns:
+        dict: The response from the server after the task completion.
+    """
+    
     r = run_gcode("M400")
     return r
 
@@ -65,13 +91,24 @@ def set_relative_cords():
     return r
 
 def home():
+    """
+    Homes the axes, moving it to its origin point.
+    """
+
     set_acceleration(200)
     wait()
     r = run_gcode(f"G28 X Y F{F}")
     wait()
     return r
 
-def move_to_points(pts):
+def move_pour_points(pts):
+    """
+    Moves the printer sequentially to a list of points and performs an pouring and cleaning action at each point.
+
+    Args:
+        pts (list of tuples): Points to move to.
+    """
+
     current_position = (max_x, max_y)
     
     while pts:
@@ -97,6 +134,14 @@ def max():
    return r
 
 def clean(iterations=10, log=True):
+    """
+    Performs a cleaning routine by moving the printer head in a specific pattern.
+
+    Args:
+        iterations (int): Number of cleaning iterations.
+        log (bool): If True, logs the cleaning process.
+    """
+
     if log:
         print('Cleaning')
     set_relative_cords()
@@ -134,7 +179,7 @@ def run():
     set_acceleration(200)
     wait()
     center_of_mug = vision_system()
-    move_to_points(center_of_mug)
+    move_pour_points(center_of_mug)
 
 if __name__ == "__main__":
     run()
