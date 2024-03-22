428d7ced8acbc840f2124bb41e1e9cf34a96af2a
wasilewskiJ <wasilewski.jakub@outlook.com>
2024-03-22 23:05:43 +0100

    Update correct.py - added function description

--- vision_system/correct/correct.py
+++ vision_system/correct/correct.py
@@ -4,9 +4,20 @@ import os
 
 
 def correct_photo(img, DIR_PATH='./vision_system/correct', CALIBRATOR_NAME='dist_pickle.p', OUTPUT=False,  OUTPUT_NAME='corrected_img.png'):
-    """ 
-    Correct the distortion in an image using calibration parameters.
     """
+    Correct the distortion in an image using pre-calibrated camera parameters.
+
+    Args:
+        img (numpy.ndarray): The image to be corrected, as a numpy array.
+        DIR_PATH (str): Directory where the calibration file is stored. Defaults to './vision_system/correct'.
+        CALIBRATOR_NAME (str): Filename of the calibration file. Defaults to 'dist_pickle.p'.
+        OUTPUT (bool): If True, the corrected image will be saved to the specified directory. Defaults to False.
+        OUTPUT_NAME (str): Name of the file to save the corrected image. Used only if OUTPUT is True. Defaults to 'corrected_img.png'.
+
+    Returns:
+        numpy.ndarray: The undistorted image as a numpy array.
+    """
+
     CALIBRATION_PATH = os.path.join(DIR_PATH, CALIBRATOR_NAME)
     OUTPUT_PATH = os.path.join(DIR_PATH, OUTPUT_NAME)
 
@@ -37,7 +48,7 @@ def correct_photo(img, DIR_PATH='./vision_system/correct', CALIBRATOR_NAME='dist
 
     # Save the corrected image if asked
     if OUTPUT:
-    	cv2.imwrite(OUTPUT_PATH, undistorted_img)
+        cv2.imwrite(OUTPUT_PATH, undistorted_img)
     
 	
     print(f"Image has been corrected.")
